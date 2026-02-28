4b7afd95a8758cf28c38aac1b4cacc815d3cb9c8
Nasrullaunais <nasrullaunais@gmail.com>
2026-03-01 01:25:03 +0530

    feat: Add navigation and context management for authentication and active chat
    - Introduced `AuthContext` for managing authentication state.
    - Implemented `BottomTabNavigator` for tab navigation with custom icons.
    - Created `NavigationService` to handle navigation actions and pending navigation.
    - Developed `RootStackNavigator` to manage the main navigation flow.
    - Defined types for navigation parameters in `types.ts`.
    - Added `ActiveChatContext` to manage the currently active chat.
    - Established utility functions in `CommonUtils` for user actions (block/unblock).
    - Created `TooltipMenu` component for displaying contextual menus.
    - Added constants and utility functions for app configuration and permissions.
    - Implemented typography management in `themeTypography.ts`.
    - Configured TypeScript settings in `tsconfig.json`.

---
+++ cometchat-builder-react-native/src/utils/AppConstants.tsx
@@ -0,0 +1,39 @@
+export const AppConstants = {
+  fcmProviderId: '',
+  apnsProviderId: '',
+  authKey: 'ffeb600c6bafd22fcbc11cee200614c2c8114f7e',
+  appId: '16758201336758e18',
+  region: 'in',
+  subscriptionType: 'ALL_USERS',
+  versionNumber: 'V5.2.2',
+  webClientId:
+    '',
+  iosClientId:
+    '',
+};
+
+export const SCREEN_CONSTANTS = {
+  LOGIN: 'Login',
+  APP_CRED: 'AppCredentials',
+  SAMPLE_USER: 'SampleUser',
+  ONGOING_CALL_SCREEN: 'OngoingCallScreen',
+  BOTTOM_TAB_NAVIGATOR: 'BottomTabNavigator',
+  CHATS: 'Chats',
+  CALLS: 'Calls',
+  USERS: 'Users',
+  GROUPS: 'Groups',
+  CONVERSATION: 'Conversation',
+  CREATE_CONVERSATION: 'CreateConversation',
+  MESSAGES: 'Messages',
+  THREAD_VIEW: 'ThreadView',
+  USER_INFO: 'UserInfo',
+  GROUP_INFO: 'GroupInfo',
+  ADD_MEMBER: 'AddMember',
+  TRANSFER_OWNERSHIP: 'TransferOwnershipSection',
+  BANNED_MEMBER: 'BannedMember',
+  VIEW_MEMBER: 'ViewMembers',
+  CALL_LOGS: 'CallLogs',
+  CALL_DETAILS: 'CallDetails',
+  AI_AGENTS: 'AIAgents',
+  QR_SCREEN: 'QRScreen',
+} as const;
